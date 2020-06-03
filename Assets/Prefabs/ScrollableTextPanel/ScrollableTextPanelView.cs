a77308de761d3268bf7d8e9e6abbdd80c8f70fbc
Jun Tachikawa <jtachikawa@xevo.com>
2020-06-03 19:43:45 +0900

    add basic Swpie handler using UniRx, ScrollableTextPanel for reusable debug compornent

---
+++ Assets/Prefabs/ScrollableTextPanel/ScrollableTextPanelView.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UniRx;
+
+public class ScrollableTextPanelView : MonoBehaviour
+{
+    [SerializeField]
+    public BoolReactiveProperty keepLatest = new BoolReactiveProperty();
+
+    ScrollRect scrollRect;
+    Scrollbar scrollbar;
+    Text text;
+
+    void Awake()
+    {
+        scrollRect = GetComponent<ScrollRect>();
+        scrollbar = scrollRect.verticalScrollbar;
+        text = GetComponentInChildren<Text>();
+    }
+
+    void Start()
+    {
+        scrollRect.OnValueChangedAsObservable()
+            .Where(_ => keepLatest.Value)
+            .Subscribe(_ => {
+                scrollRect.verticalNormalizedPosition = 0;
+            }).AddTo(this);
+
+        keepLatest.SkipLatestValueOnSubscribe().Subscribe(v => {
+            scrollbar.interactable = !v;
+            if(v) {
+                scrollRect.verticalNormalizedPosition = 0;
+            }
+        });
+    }
+
+    public void WriteLine(string msg) 
+    {
+        text.text += $"{msg}\n";
+    }
+
+    public void Clear()
+    {
+        text.text = "";
+        text.SetAllDirty();
+    }
+}
--- Assets/Scripts/Swipe.cs
+++ Assets/Scripts/Swipe.cs
@@ -1,18 +1,75 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.EventSystems;
+using UniRx;
+using UniRx.Triggers;
 
 public class Swipe : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    public Toggle KeepLatestToggle;
+    public Button DebugButton;
+    public Button ClearButton;
+    public ScrollableTextPanelView logPanel;
+
+    void log(string msg)
     {
-        
+        logPanel.WriteLine(msg);
     }
 
-    // Update is called once per frame
-    void Update()
+    void Start()
     {
-        
+        bool begin = false;
+        var beginPos = Vector2.zero;
+        var beginTime = DateTime.Now;
+        var points = 0;
+
+        this.UpdateAsObservable()
+            .Where(_ => Input.touchCount > 0)
+            .Select(_ => Input.GetTouch(0))
+            .Where(touch => touch.phase == TouchPhase.Began)
+            .Where(touch => !EventSystem.current.IsPointerOverGameObject(touch.fingerId)) // ignore on UI
+            .Subscribe(touch => {
+                begin = true;
+                beginPos = touch.position;
+                beginTime = DateTime.Now;
+                log($"<color=red>[0] {beginTime} {touch.phase}: {touch.position}</color>");
+                points = 1;
+            });
+
+        this.UpdateAsObservable()
+            .Where(_ => begin && Input.touchCount > 0)
+            .Select(_ => Input.GetTouch(0))
+            .Where(touch => (touch.phase != TouchPhase.Began))
+            .Subscribe(touch =>
+            {
+                if (touch.phase == TouchPhase.Ended || touch.phase == TouchPhase.Canceled)
+                {
+                    begin = false; // clear flag
+                }
+                log($"[{points}] {DateTime.Now} {touch.phase}: {touch.position}");
+                points++;
+                if (touch.phase == TouchPhase.Ended)
+                {
+                    var vec = touch.position - beginPos;
+                    log($"<color=red>Swpie: vec={vec}, {points} points, {(DateTime.Now - beginTime).Milliseconds} ms</color>");
+                }
+            });
+
+        // Debug
+        int count = 0;
+        GameObject.Find("Button")?.GetComponent<Button>()?.OnClickAsObservable()
+            .Subscribe(_ => { log($"button-{count}"); count++; });
+
+        KeepLatestToggle.OnValueChangedAsObservable()
+            .Subscribe(v => logPanel.keepLatest.Value = v);
+
+        DebugButton.OnClickAsObservable()
+            .Subscribe(_ => log("Debug"));
+
+        ClearButton.OnClickAsObservable()
+            .Subscribe(_ => logPanel.Clear());
     }
 }
