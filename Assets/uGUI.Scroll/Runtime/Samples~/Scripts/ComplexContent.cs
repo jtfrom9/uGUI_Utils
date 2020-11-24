a22c610e56ec4ec0940140e8e555145600d1df26
jtfrom9 <jun.tachikawa@gmail.com>
2020-11-25 00:35:43 +0900

    add complex scroll content example

---
+++ Assets/uGUI.Scroll/Runtime/Samples~/Scripts/ComplexContent.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UniRx;
+using uGUI.Scroll;
+
+public class ComplexContent : MonoBehaviour
+{
+    IScrollRectContent content = null;
+
+    [SerializeField] Toggle toggle = default;
+
+    void Start()
+    {
+        content = GetComponent<IScrollRectContent>();
+
+        toggle.OnValueChangedAsObservable().Subscribe(v => {
+            Debug.Log($"{this.name} checked. {v}");
+        }).AddTo(this);
+    }
+}
---
+++ Assets/uGUI.Scroll/Runtime/Samples~/Scripts/ScrollRectComplexContent.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.EventSystems;
+using uGUI.Scroll;
+using UniRx;
+
+public class ScrollRectComplexContent : MonoBehaviour
+{
+    [SerializeField] Button addButton = default;
+    [SerializeField] GameObject itemPrefab = default;
+    [SerializeField] VerticalScrollRectController verticalScrollRectController = default;
+
+    int count = 0;
+    void Start()
+    {
+        addButton.OnClickAsObservable().Subscribe(_ =>
+        {
+            var go = Instantiate(itemPrefab);
+            go.name = $"Added: {count}";
+            var content = go.GetComponent<IScrollRectContent>();
+            verticalScrollRectController.AddContent(content);
+            count++;
+        }).AddTo(this);
+    }
+}
